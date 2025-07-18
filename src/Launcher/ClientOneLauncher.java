36080f7a2a96956df8d7e448e6b4e08ecbce7de3
Jerry Dev <jerryarthur075@gmail.com>
2025-07-18 07:27:28 +0100

    Cleaned up comments in Launcher classes

--- src/Launcher/ClientOneLauncher.java
+++ src/Launcher/ClientOneLauncher.java
@@ -8,15 +8,17 @@ public class ClientOneLauncher {
 
     public static void main(String[] args)
     {
-        TicTacToeClient application; //declare client application
+        TicTacToeClient application;
 
-        //if no command line args
+
         if(args.length == 0)
         {
+            // Use default localhost if no IP is passed
             application = new TicTacToeClient("127.0.0.1");
         }
         else
         {
+            // Use IP argument from command line
             application = new TicTacToeClient(args[0]); //use args
         }
 
--- src/Launcher/ClientTwoLauncher.java
+++ src/Launcher/ClientTwoLauncher.java
@@ -8,16 +8,17 @@ public class ClientTwoLauncher {
 
     public static void main(String[] args)
     {
-        TicTacToeClient application; //declare client application
+        TicTacToeClient application;
 
-        //if no command line args
+
         if(args.length == 0)
         {
+            // Use default localhost if no IP is passed
             application = new TicTacToeClient("127.0.0.1");
         }
         else
         {
-            application = new TicTacToeClient(args[0]); //use args
+            application = new TicTacToeClient(args[0]); // Use IP argument from command line
         }
 
         application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
